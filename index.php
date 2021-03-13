a1ca48b7d52186ce2c87ee5f612d123c4e61eb57
DEVMUNDS <devmunds@gmail.com>
2021-03-13 00:14:59 -0300

    Update code

--- index.php
+++ index.php
@@ -2,15 +2,7 @@
 /*
 * URL's Obrigatórias para fazer contação e transforma a cotação em um pedido com API FreteClick
 */
-$url_api                          = 'https://api.freteclick.com.br/quotes';
-$url_shipping_quote               = '/sales/shipping-quote.json';
-$url_city_origin                  = '/carrier/search-city-origin.json';
-$url_city_destination             = '/carrier/search-city-destination.json';
-$url_search_city_from_cep         = '/carrier/search-city-from-cep.json';
-$url_choose_quote                 = '/sales/choose-quote.json';
-$url_add_quote_destination_client = '/sales/add-quote-destination-client.json';
-$url_add_quote_origin_company     = '/sales/add-quote-origin-company.json.json';
-
+$url_api  = 'https://api.freteclick.com.br/quotes';
 
 // Chave para realizar cotação com platafoma
 $api_token = '242c5d6f05fd292bc91fd67170dc5a04';
